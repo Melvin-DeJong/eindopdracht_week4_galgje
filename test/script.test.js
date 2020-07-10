26eab3ec6eb9f8f05c56cf0963bf035e41c5588a
Melvin de Jong <melvindejong22@gmail.com>
2020-07-10 09:28:54 +0200

    alles in mappen gezet

---
+++ test/script.test.js
@@ -0,0 +1,19 @@
+const { TestScheduler } = require("jest")
+
+
+
+
+
+
+// updaten van de lijst met letters die al zijn geraden door de gebruiker
+describe("Test if guessed letter is added to array", () => {
+    test("")
+});
+
+
+// verliezen van de game wanneer er geen pogingen meer over zijn
+describe("Test if ")
+
+
+
+// winnen van de game
