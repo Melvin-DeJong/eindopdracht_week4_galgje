75e68b71f347d460c0bbf67c74207b2229dc0451
jelmerpeterson <jelmerpeterson11@gmail.com>
2020-07-10 11:46:54 +0200

    nieuwe tests jelmer

--- test/script.test.js
+++ test/script.test.js
@@ -25,13 +25,3 @@ describe("When player guesed a word update tries", () => {
   let tries = 3;
   expect(increaseTries()).toBe(4);
 });
-
-// updaten van de lijst met letters die al zijn geraden door de gebruiker
-describe("Test if guessed letter is added to array", () => {
-  test("");
-});
-
-// verliezen van de game wanneer er geen pogingen meer over zijn
-describe("Test if ");
-
-// winnen van de game
